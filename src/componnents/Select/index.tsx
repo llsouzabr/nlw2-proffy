de9d9881787b0952c8db56597ad13ade9851bb28
Leonardo Souza <llsouza.br@gmail.com>
2020-08-05 12:47:42 -0300

    Correção componentes

--- src/componnents/Select/index.tsx
+++ src/componnents/Select/index.tsx
@@ -17,7 +17,7 @@ const Select:React.FC<SelectProps> = ({label, name, options, ...rest }) => {
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
             <select id={name} {...rest}>
-                <option value="" disabled selected hidden>Selecione uma opção</option>
+                <option defaultValue="" value="" disabled hidden>Selecione uma opção</option>
                 {options.map(option => {
                     return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
